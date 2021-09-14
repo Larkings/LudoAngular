ee9bccdcbfa1a2ae96e1f56ea808fe3fbe731089
huynhkt <bao.huynh@hva.nl>
2021-09-14 16:17:13 +0200

    first setup landing page

--- mensErgerJeNiet/src/app/app.module.ts
+++ mensErgerJeNiet/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './mainpage/header/header.component';
 import { SidebarComponent } from './mainpage/sidebar/sidebar.component';
 import { NavbarComponent } from './mainpage/navbar/navbar.component';
 import { HomeComponent } from './mainpage/home/home.component';
+import {MaterialModule} from "./material/material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { HomeComponent } from './mainpage/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mensErgerJeNiet/src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+
+const MaterialComponents =[
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule
+]
+
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+
+export class MaterialModule { }
