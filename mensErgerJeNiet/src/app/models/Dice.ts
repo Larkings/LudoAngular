f853cbbe7d5ac5e0b9f893aa35e14d7212bc746d
Dalvin Newland <newlank@outlook.com>
2021-11-07 14:27:18 +0100

    add pawns & methodes

--- mensErgerJeNiet/src/app/models/Dice.ts
+++ mensErgerJeNiet/src/app/models/Dice.ts
@@ -6,11 +6,14 @@ export class Dice {
   constructor(public result: number) {
   }
 
-  //roll dice and returns a random integer number between 1-6
+  /**
+   * roll dice and returns a random integer number between 1-6
+   */
   public static rollDice(): number {
     let randomNumber = getRandomInt(1, 6);
     return randomNumber;
   }
+
 
   public getDiceValue(){
     return this.result;
