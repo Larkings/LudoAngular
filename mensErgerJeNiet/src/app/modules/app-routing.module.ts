b39a8662383de1dfd7f26bb8d14322c169147d41
Dalvin Newland <newlank@outlook.com>
2021-11-10 17:27:09 +0100

    fixed routing & code clean up

--- mensErgerJeNiet/src/app/modules/app-routing.module.ts
+++ mensErgerJeNiet/src/app/modules/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BoardComponent} from "../shared/components/board/board.component";
-
+import {HomeComponent} from "../shared/components/home/home.component"
 
 const routes: Routes = [
-  {path: 'board', component: BoardComponent},
+  {path: 'playgame', component: BoardComponent},
+  {path: 'home', component: HomeComponent},
+  // {path: 'settings', component: BoardComponent},
+  // {path: 'about', component: BoardComponent},
+
 ];
 
 @NgModule({
