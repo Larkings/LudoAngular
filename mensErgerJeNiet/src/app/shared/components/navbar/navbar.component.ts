c5d010cc665c74415676167a1ed181ce5d276c0b
Larkings <58095625+Larkings@users.noreply.github.com>
2021-12-14 20:34:58 +0100

    Merge done by lloyd

--- mensErgerJeNiet/src/app/shared/components/navbar/navbar.component.ts
+++ mensErgerJeNiet/src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalService} from "../../../service/modal.service";
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+/*providers: [ModalService]*/
 })
 export class NavbarComponent implements OnInit {
 
