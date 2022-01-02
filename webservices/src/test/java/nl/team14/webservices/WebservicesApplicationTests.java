19f4c69459fbca054a026a2e7c5918d2c71b9cf5
Dalvin Newland <newlank@outlook.com>
2022-01-02 02:40:07 +0100

    added back-end module

---
+++ webservices/src/test/java/nl/team14/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.team14.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
